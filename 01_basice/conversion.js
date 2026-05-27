3ff71d09515183c60a040ae0ce1a3a4c0b3b7583
Arvindsin6386 <anujreoti7398@gmail.com>
2026-05-27 10:15:47 +0000

    some topic is add

---
+++ 01_basice/conversion.js
@@ -0,0 +1,29 @@
+let countNumber = 99
+console.log(typeof countNumber);
+
+let conversion = "99abd"
+console.log(typeof conversion)
+let valueInNumber = Number(conversion)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/* 
+this line of code means that when we define a String and convert into a Number and after check
+its datatype its give a number and and its value show NAN --- means [Not A Number] 
+*/
+
+/*
+NOTE --- giving a NUll is value is = 0
+and Giving Unefine its value is = NAN
+ */
+
+// ********** coversion operation *************
+
+console.log("1" + 2) // output is 12
+console.log("1"+ 2 + 3) // output is 123
+console.log(1 + 3 + "3") // output is 43
+ /*
+ NOTE when we add string the output is print as same not add
+ and you give the string in last then first add and print as well as that have mention in above example
+ 
+ */
---
+++ 01_basice/variable.js
@@ -0,0 +1,26 @@
+const accountId = 123456
+let accountEmail = "anuj@test.com"
+console.table([
+    accountId ,accountEmail
+]);
+ /*
+always prefer using let and use var because of issue of scope and function cope 
+const -- cannot be change one we define its 
+let --- change its after define 
+ */
+
+
+/* -- DATATPYE-- 
+1 - Number 
+2-  String -> like this ""
+3-  boolean -> ture oR False
+4-  null -> Standalond value and when we check the datatype like this console.log(typeof null) its give the object
+5- undefine -> and we check the undefine its give undefine 
+and the main different between of null and undefine is for Example
+      when we write  let accountid;-> the output of this code is undefine 
+       and we write let accountId = null -> their output is empty and give any value 
+
+5- symbole -> unique
+6 - object -> 
+and the Note is when we check the datatype using [typeof]----
+*/
