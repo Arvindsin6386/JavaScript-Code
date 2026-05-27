ccc7e8ec7cb452d7396a23cab043d4137c7bc7e0
Arvindsin6386 <anujreoti7398@gmail.com>
2026-05-27 12:51:15 +0000

    some add in conversion

--- 01_basice/conversion.js
+++ 01_basice/conversion.js
@@ -17,13 +17,40 @@ NOTE --- giving a NUll is value is = 0
 and Giving Unefine its value is = NAN
  */
 
-// ********** coversion operation *************
+               // ********** coversion operation *************
 
-console.log("1" + 2) // output is 12
-console.log("1"+ 2 + 3) // output is 123
-console.log(1 + 3 + "3") // output is 43
+                   console.log("1" + 2) // output is 12
+                   console.log("1"+ 2 + 3) // output is 123
+                   console.log(1 + 3 + "3") // output is 43
  /*
  NOTE when we add string the output is print as same not add
  and you give the string in last then first add and print as well as that have mention in above example
  
  */
+
+
+                   // ******* STRING *************
+
+               const name = "Anuj Singh"
+                const address = " Lucknow"
+
+                  console.log(`this is my name ${name} and my address is ${address}`)
+
+      /*
+                 when we add two string using in mordern ways like using String Interpolation
+         */
+
+
+                         const gameName = new String("anujsingh")
+                         console.log(gameName)
+                         console.log(gameName.toLowerCase())
+                         console.log(gameName.toUpperCase())
+                         console.log(gameName.slice(-8 ,4))
+                         console.log(gameName.substring(2 ,10))
+
+
+
+                  // ****** Maths *********
+                  console.log(Math)
+                  console.log(Math.random())
+                  console.log(Math.floor(Math.random()))
